d76dad8c8cb720e53b6bdb6037c9ac203d3e4efa
Parth Gadoo <p.gadoo@iitg.ac.in>
2022-12-13 09:49:25 +0530

    bug_fixed

--- MovingDot/MovingDot.java
+++ MovingDot/MovingDot.java
@@ -34,6 +34,8 @@ public class MovingDot{
     }
     class MyDrawPanel extends JPanel{
         public void paintComponent(Graphics g){
+            g.setColor(Color.white);
+            g.fillRect(0,0,this.getWidth(), this.getHeight());
             g.setColor(Color.green);
             g.fillOval(x,y,40,40);
         }
